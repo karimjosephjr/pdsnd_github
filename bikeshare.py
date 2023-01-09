25d9cb3e71e725558f9dd27ede47885e925726c3
Karim Joseph, Jr <info@karimjosephjr.com>
2023-01-08 21:50:42 -0700

    Update single vs double quote usage for consistency.

--- bikeshare.py
+++ bikeshare.py
@@ -97,7 +97,7 @@ def validate_user_input(request, valid_inputs, trim=0):
 
     while output not in valid_list:
         if attempted:
-            print("Unable to find a match. Please try again.")
+            print('Unable to find a match. Please try again.')
         user_input = input(request).strip().lower()
         if user_input == 'options':
             attempted = False
@@ -120,16 +120,16 @@ def get_filters():
     Asks user to specify a city, month, and day to analyze
     :return: (city, month, day) to be analyzed
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print("Hello! Let's explore some US bikeshare data!")
     print("Type 'options' for options at any time.")
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-    city = validate_user_input("\nWould you like to review Chicago, New York City, or Washington data? ", CITIES)
+    city = validate_user_input('\nWould you like to review Chicago, New York City, or Washington data? ', CITIES)
 
     # get user input for month (all, january, february, ... , june)
-    month = validate_user_input("\nWhat month would you like to review? ", MONTHS, 3)
+    month = validate_user_input('\nWhat month would you like to review? ', MONTHS, 3)
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
-    day = validate_user_input("\nWhat day of the week would you like to review? ", DAYS, 3)
+    day = validate_user_input('\nWhat day of the week would you like to review? ', DAYS, 3)
 
     print('-' * 40)
     return city, month, day
@@ -139,8 +139,8 @@ def load_data(city, month, day):
     """
     Loads data for the specified city, filters by month and day if applicable, and adds columns for easy query
     :param city: name of the city to analyze
-    :param month: name of the month to filter by, or "All Months" to apply no month filter
-    :param day: name of the day of week to filter by, or "All Days" to apply no day filter
+    :param month: name of the month to filter by, or 'All Months' to apply no month filter
+    :param day: name of the day of week to filter by, or 'All Days' to apply no day filter
     :return: Pandas DataFrame containing city data filtered by month and day
     """
 
@@ -148,24 +148,24 @@ def load_data(city, month, day):
     df = pd.read_csv(CITY_DATA[city])
 
     # Setup columns for filtering
-    df["Start Time"] = pd.to_datetime(df["Start Time"])
-    df["Month of Year"] = df["Start Time"].dt.month_name()
-    df["Day of Week"] = df["Start Time"].dt.day_name()
-    df["Start Hour"] = df["Start Time"].dt.hour
-    df["End Time"] = pd.to_datetime(df["End Time"])
+    df['Start Time'] = pd.to_datetime(df['Start Time'])
+    df['Month of Year'] = df['Start Time'].dt.month_name()
+    df['Day of Week'] = df['Start Time'].dt.day_name()
+    df['Start Hour'] = df['Start Time'].dt.hour
+    df['End Time'] = pd.to_datetime(df['End Time'])
 
     # Filter using provided parameters and extra columns
-    if month != "All Months":
-        df = df[df["Month of Year"] == month]
-    if day != "All Days":
-        df = df[df["Day of Week"] == day]
+    if month != 'All Months':
+        df = df[df['Month of Year'] == month]
+    if day != 'All Days':
+        df = df[df['Day of Week'] == day]
 
     # Convenience column for station stats
-    df["Start and End Station"] = df["Start Station"].str.cat(df["End Station"], sep=" - ")
+    df['Start and End Station'] = df['Start Station'].str.cat(df['End Station'], sep=' - ')
 
     # Clean Birth Year if it's even available
-    if "Birth Year" in df.columns:
-        df["Birth Year"] = df["Birth Year"].astype("Int64")
+    if 'Birth Year' in df.columns:
+        df['Birth Year'] = df['Birth Year'].astype('Int64')
 
     return df
 
@@ -178,21 +178,21 @@ def time_stats(df):
 
     try:
         # display the most common month
-        months = df["Month of Year"].mode()
-        print("Most Common Month: {}".format(months.to_numpy()))
+        months = df['Month of Year'].mode()
+        print('Most Common Month: {}'.format(months.to_numpy()))
 
         # display the most common day of week
-        day_of_week = df["Day of Week"].mode()
-        print("Most Common Day of Week: {}".format(day_of_week.to_numpy()))
+        day_of_week = df['Day of Week'].mode()
+        print('Most Common Day of Week: {}'.format(day_of_week.to_numpy()))
 
         # display the most common start hour
-        start_hour = df["Start Hour"].mode()
-        print("Most Common Start Hour: {}".format(start_hour.to_numpy()))
+        start_hour = df['Start Hour'].mode()
+        print('Most Common Start Hour: {}'.format(start_hour.to_numpy()))
     except Exception as err:
-        print("Error collecting frequency stats. Moving on.\n")
-        print(f"Unexpected {err=}, {type(err)=}")
+        print('Error collecting frequency stats. Moving on.\n')
+        print(f'Unexpected {err=}, {type(err)=}')
 
-    print("\nThis took {} seconds.".format(time.time() - start_time))
+    print('\nThis took {} seconds.'.format(time.time() - start_time))
     print('-' * 40)
 
 
@@ -204,21 +204,21 @@ def station_stats(df):
 
     try:
         # display most commonly used start station
-        start_location = df["Start Station"].mode()
-        print("Most Common Start Location: {}".format(start_location.to_numpy()))
+        start_location = df['Start Station'].mode()
+        print('Most Common Start Location: {}'.format(start_location.to_numpy()))
 
         # display most commonly used end station
-        end_location = df["End Station"].mode()
-        print("Most Common End Location: {}".format(end_location.to_numpy()))
+        end_location = df['End Station'].mode()
+        print('Most Common End Location: {}'.format(end_location.to_numpy()))
 
         # display most frequent combination of start station and end station trip
-        location_combo = df["Start and End Station"].mode()
+        location_combo = df['Start and End Station'].mode()
         print("Most Common 'Start - End' Locations: {}".format(location_combo.to_numpy()))
     except Exception as err:
-        print("Error collecting station stats. Moving on.\n")
-        print(f"Unexpected {err=}, {type(err)=}")
+        print('Error collecting station stats. Moving on.\n')
+        print(f'Unexpected {err=}, {type(err)=}')
 
-    print("\nThis took {} seconds.".format(time.time() - start_time))
+    print('\nThis took {} seconds.'.format(time.time() - start_time))
     print('-' * 40)
 
 
@@ -230,17 +230,17 @@ def trip_duration_stats(df):
 
     try:
         # display total travel time
-        total_travel = df["Trip Duration"].sum() / 60
-        print("Total Travel Time (in Minutes): {}".format(total_travel))
+        total_travel = df['Trip Duration'].sum() / 60
+        print('Total Travel Time (in Minutes): {}'.format(total_travel))
 
         # display mean travel time
-        average_travel = df["Trip Duration"].mean() / 60
-        print("Average Travel Time (in Minutes): {}".format(average_travel))
+        average_travel = df['Trip Duration'].mean() / 60
+        print('Average Travel Time (in Minutes): {}'.format(average_travel))
     except Exception as err:
-        print("Error collecting duration stats. Moving on.\n")
-        print(f"Unexpected {err=}, {type(err)=}")
+        print('Error collecting duration stats. Moving on.\n')
+        print(f'Unexpected {err=}, {type(err)=}')
 
-    print("\nThis took {} seconds.".format(time.time() - start_time))
+    print('\nThis took {} seconds.'.format(time.time() - start_time))
     print('-' * 40)
 
 
@@ -252,38 +252,38 @@ def user_stats(df):
 
     try:
         # Display counts of user types
-        user_type_counts = df["User Type"].value_counts()
-        print("User Counts by Account Type:")
+        user_type_counts = df['User Type'].value_counts()
+        print('User Counts by Account Type:')
         print(user_type_counts.to_string())
     except Exception as err:
-        print("Error collecting account type stats. Moving on.\n")
-        print(f"Unexpected {err=}, {type(err)=}")
+        print('Error collecting account type stats. Moving on.\n')
+        print(f'Unexpected {err=}, {type(err)=}')
 
     # Display counts of gender
-    print("\nUser Counts by Gender:")
+    print('\nUser Counts by Gender:')
     try:
-        gender_counts = df["Gender"].value_counts()
+        gender_counts = df['Gender'].value_counts()
         print(gender_counts.to_string())
     except KeyError:
-        print("Gender is not available for this given set of data.")
+        print('Gender is not available for this given set of data.')
     except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
+        print(f'Unexpected {err=}, {type(err)=}')
 
     # Display earliest, most recent, and most common year of birth
-    print("\nBirth Year Stats ->")
+    print('\nBirth Year Stats ->')
     try:
-        earliest = df["Birth Year"].min()
-        latest = df["Birth Year"].max()
-        mode = df["Birth Year"].mode()
-        print("Earliest Birth Year: {}".format(earliest))
-        print("Latest Birth Year: {}".format(latest))
-        print("Most Common Birth Year: {}".format(mode.to_numpy()))  # to_numpy()
+        earliest = df['Birth Year'].min()
+        latest = df['Birth Year'].max()
+        mode = df['Birth Year'].mode()
+        print('Earliest Birth Year: {}'.format(earliest))
+        print('Latest Birth Year: {}'.format(latest))
+        print('Most Common Birth Year: {}'.format(mode.to_numpy()))  # to_numpy()
     except KeyError:
-        print("Birth Year is not available for this given set of data.")
+        print('Birth Year is not available for this given set of data.')
     except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
+        print(f'Unexpected {err=}, {type(err)=}')
 
-    print("\nThis took {} seconds.".format(time.time() - start_time))
+    print('\nThis took {} seconds.'.format(time.time() - start_time))
     print('-' * 40)
 
 
@@ -291,8 +291,8 @@ def show_raw_data(df):
     index = 0
     while True and len(df.index) > 0:
         df = df.iloc[index * 5:, :]
-        print_dataset = input("\nWould you like to review (the next) 5 records of the dataset [y/n]? ")
-        if len(print_dataset) > 0 and print_dataset[:1].lower() == "y":
+        print_dataset = input('\nWould you like to review (the next) 5 records of the dataset [y/n]? ')
+        if len(print_dataset) > 0 and print_dataset[:1].lower() == 'y':
             if len(df.index) > 0:
                 print(df.head(5).to_string())
             index += 1
@@ -315,5 +315,5 @@ def main():
             break
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
